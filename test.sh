6e274727ce55121916123ed5f055921448efc776
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-05-05 15:01:47 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sed -i '/export BUNDLR_ADDRESS=<YOUR_ADDRESS>/d' ~/.bash_profile
-CASCADIA_ADDRESS=$($HOME/go/bin/cascadiad keys show wallet -a)
-CASCADIA_VALOPER=$($HOME/go/bin/cascadiad keys show wallet --bech val -a)
-echo 'export CASCADIA_ADDRESS='${CASCADIA_ADDRESS} >> $HOME/.bash_profile
-echo 'export CASCADIA_VALOPER='${CASCADIA_VALOPER} >> $HOME/.bash_profile
-source $HOME/.bash_profile
+# Install NVM on Ubuntu
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+source ~/.bashrc
+nvm install node
+
+# Register Validator and Stake
+sudo npm i -g @bundlr-network/testnet-cli@latest
