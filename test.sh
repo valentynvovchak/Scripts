c4ebe2c0ba6688683c64b469b8058cd3c2152b08
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-05-05 14:26:05 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sed -i '/export BUNDLR_ADDRESS=<YOUR_ADDRESS>/d' ~/.bash_profile
-CASCADIA_ADDRESS=\$(cascadiad keys show wallet -a)
-CASCADIA_VALOPER=\$(cascadiad keys show wallet --bech val -a)
+CASCADIA_ADDRESS=$(cascadiad keys show wallet -a)
+CASCADIA_VALOPER=$(cascadiad keys show wallet --bech val -a)
 echo 'export CASCADIA_ADDRESS='${CASCADIA_ADDRESS} >> $HOME/.bash_profile
 echo 'export CASCADIA_VALOPER='${CASCADIA_VALOPER} >> $HOME/.bash_profile
 source $HOME/.bash_profile
