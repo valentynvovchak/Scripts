382c446753d864d74ada5e00727fb10153f748b9
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-05-08 12:10:24 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,10 +10,10 @@ nvm install node
 
 BUNDLER_PORT=42069
 BUNDLER_URL=https://testnet1.bundlr.network
-echo 'export GW_CONTRACT="RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA"' >> ~/.bash_profile
+echo 'export GW_CONTRACT="RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA"' >> $HOME/.bash_profile
 GW_ARWEAVE=https://arweave.testnet1.bundlr.network
 GW_STATE_ENDPOINT=https://faucet.testnet1.bundlr.network
-source ~/.bash_profile
+source $HOME/.bash_profile
 
 tee .env > /dev/null <<EOF
 PORT=$BUNDLER_PORT
