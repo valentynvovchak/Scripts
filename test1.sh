a58ecaa56f539758f299f6ae1d2bb43f686e3ec0
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-05-08 12:12:52 +0300

    Create test1.sh

---
+++ test1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install NVM on Ubuntu
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+source ~/.bashrc
+nvm install node
+
+# Check ballance
+#npx @bundlr-network/testnet-cli@latest balance $BUNDLR_ADDRESS
+
+BUNDLER_PORT=42069
+BUNDLER_URL=https://testnet1.bundlr.network
+echo 'export GW_CONTRACT="RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA"' >> $HOME/.bash_profile
+GW_ARWEAVE=https://arweave.testnet1.bundlr.network
+GW_STATE_ENDPOINT=https://faucet.testnet1.bundlr.network
+source $HOME/.bash_profile
+
+tee .env > /dev/null <<EOF
+PORT=$BUNDLER_PORT
+BUNDLER_URL=$BUNDLER_URL
+GW_CONTRACT=$GW_CONTRACT
+GW_ARWEAVE=$GW_ARWEAVE
+GW_STATE_ENDPOINT=$GW_STATE_ENDPOINT
+EOF
