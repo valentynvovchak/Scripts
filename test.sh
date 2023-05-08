cb6c107dcb7511ac57551de5ea12f40b5d467273
Valentyn Vovchak <52880937+valentynvovchak@users.noreply.github.com>
2023-05-08 12:05:08 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,4 +6,19 @@ source ~/.bashrc
 nvm install node
 
 # Check ballance
-npx @bundlr-network/testnet-cli@latest balance $BUNDLR_ADDRESS
+#npx @bundlr-network/testnet-cli@latest balance $BUNDLR_ADDRESS
+
+BUNDLER_PORT=42069
+BUNDLER_URL=https://testnet1.bundlr.network
+echo 'export GW_CONTRACT="RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA"'
+echo 'export GW_ARWEAVE=https://arweave.testnet1.bundlr.network'
+echo 'export GW_STATE_ENDPOINT=https://faucet.testnet1.bundlr.network'
+source ~/.bash_profile
+
+tee .env > /dev/null <<EOF
+PORT=$BUNDLER_PORT
+BUNDLER_URL=$BUNDLER_URL
+GW_CONTRACT=$GW_CONTRACT
+GW_ARWEAVE=$GW_ARWEAVE
+GW_STATE_ENDPOINT=$GW_STATE_ENDPOINT
+EOF
